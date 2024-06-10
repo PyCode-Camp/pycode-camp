bb24502a24733d47e6a623af2fd2442da919f968
s-kvng <kvngnathan8420@gmail.com>
2024-06-10 16:34:53 +0000

    comp: add card component

--- app/(root)/about/page.jsx
+++ app/(root)/about/page.jsx
@@ -1,7 +1,18 @@
+import TeamCard from "@/component/cards/TeamCard";
+import TeamCards from "@/component/cards/TeamCards";
 import React from "react";
 
+import { team } from "@/constants";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <div>About</div>
+      <div className="w-52">
+        <TeamCard index={2} item={team[0]} />
+      </div>
+    </>
+  );
 };
 
 export default About;
--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Layout = ({ children }) => {
   return (
     <>
-      <main>{children}</main>
+      <main className="min-h-screen ">{children}</main>
     </>
   );
 };
